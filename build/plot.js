e3323f47fec08a22502be009f935d3ad143b76a7
Jens birkbak aagaard <jensbaagaard@gmail.com>
2019-08-28 23:28:15 +0200

    comments

--- build/plot.js
+++ build/plot.js
@@ -1,24 +1,11 @@
 var plot = (function () {
     'use strict';
 
-    var NodeType;
-    (function (NodeType) {
-        NodeType[NodeType["Paragraph"] = 1] = "Paragraph";
-        NodeType[NodeType["Command"] = 2] = "Command";
-        NodeType[NodeType["Text"] = 3] = "Text";
-        NodeType[NodeType["Block"] = 4] = "Block";
-        NodeType[NodeType["Html"] = 5] = "Html";
-        NodeType[NodeType["Node"] = 6] = "Node";
-        NodeType[NodeType["Dictionary"] = 11] = "Dictionary";
-        NodeType[NodeType["Array"] = 12] = "Array";
-        NodeType[NodeType["Number"] = 13] = "Number";
-        NodeType[NodeType["String"] = 14] = "String";
-    })(NodeType || (NodeType = {}));
-
     var pkg = {
         name: 'plot',
         commands: {
             plot: function (dictionaryNode) {
+                console.log("Drawing..");
                 var _a = dictionaryNode.value, type = _a.type, y = _a.y, ylabel = _a.ylabel, xlabel = _a.xlabel;
                 var canvas = nxtx.htmlLite("canvas", {});
                 var ctx = canvas.getContext("2d");
@@ -28,12 +15,13 @@ var plot = (function () {
                 canvas.style.width = "400px";
                 var normalizedY = normalize(y);
                 ctx.beginPath();
-                for (var i = 0; i < y.length - 1; i++) {
-                    ctx.moveTo(i, y[i] * canvasHeight);
-                    ctx.lineTo(i + 1, y[i + 1] * canvasHeight);
+                for (var i = 0; i < normalizedY.length - 1; i++) {
+                    ctx.moveTo(i, normalizedY[i] * canvasHeight);
+                    ctx.lineTo(i + 1, normalizedY[i + 1] * canvasHeight);
                     ctx.stroke();
                 }
-                return { type: NodeType.Node, value: canvas };
+                console.log("Done drawing");
+                return canvas;
             }
         },
     };
@@ -42,7 +30,7 @@ var plot = (function () {
         var max = Math.max.apply(Math, array) - min;
         return array.map(function (e) { return (e - min) / max; });
     }
-    if (nxtx !== undefined)
+    if (nxtx)
         nxtx.registerPackage(pkg);
 
     return pkg;
