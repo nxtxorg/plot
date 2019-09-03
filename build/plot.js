02429d61ca1477edc3def091f2cfeec76a156164
Jens birkbak aagaard <jensbaagaard@gmail.com>
2019-09-03 22:30:05 +0200

    working :))

--- build/plot.js
+++ build/plot.js
@@ -8,23 +8,31 @@ var plot = (function () {
                 console.log("Drawing..");
                 var _a = dictionaryNode.value, type = _a.type, y = _a.y, ylabel = _a.ylabel, xlabel = _a.xlabel;
                 var canvas = nxtx.htmlLite("canvas", {});
+                var yValue = dictionaryNode.value.y.value.map(function (e) { return e.value; });
+                console.log(yValue);
                 var ctx = canvas.getContext("2d");
-                var canvasHeight = canvas.height = 100;
-                canvas.width = 100;
-                canvas.style.height = "200px";
-                canvas.style.width = "400px";
-                var normalizedY = normalize(y);
+                var canvasHeight = canvas.height = 200;
+                var canvasWidth = canvas.width = 400;
+                canvas.style.margin = "auto";
+                canvas.style.display = "block";
+                var normalizedY = normalize(yValue);
+                var spacing = canvasWidth / (normalizedY.length - 1);
+                console.log("spacing ", spacing);
                 ctx.beginPath();
                 for (var i = 0; i < normalizedY.length - 1; i++) {
-                    ctx.moveTo(i, normalizedY[i] * canvasHeight);
-                    ctx.lineTo(i + 1, normalizedY[i + 1] * canvasHeight);
-                    ctx.stroke();
+                    line(ctx, i * spacing, canvasHeight - (normalizedY[i] * canvasHeight), (i + 1) * spacing, canvasHeight - (normalizedY[i + 1] * canvasHeight));
                 }
+                line(ctx, 30, 0, 30, canvasHeight - 30);
                 console.log("Done drawing");
                 return canvas;
             }
         },
     };
+    function line(context, x1, y1, x2, y2) {
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+    }
     function normalize(array) {
         var min = Math.min.apply(Math, array);
         var max = Math.max.apply(Math, array) - min;
--- src/plot.ts
+++ src/plot.ts
@@ -10,28 +10,41 @@ const pkg: Package = {
             let {type, y, ylabel, xlabel} = dictionaryNode.value;
             const canvas = <HTMLCanvasElement>nxtx.htmlLite("canvas", {});
 
+            const yValue = dictionaryNode.value.y.value.map(e => e.value);
+
+            console.log(yValue);
             const ctx = canvas.getContext("2d");
 
-            const canvasHeight = canvas.height = 100;
-            canvas.width = 100;
+            const canvasHeight = canvas.height = 200;
+            const canvasWidth = canvas.width = 400;
 
-            canvas.style.height = "200px";
-            canvas.style.width = "400px";
+            canvas.style.margin = "auto";
+            canvas.style.display = "block";
+            const normalizedY = normalize(yValue);
 
-            const normalizedY = normalize(y);
+            const spacing =canvasWidth / (normalizedY.length-1);
+            console.log("spacing ",spacing);
 
             ctx.beginPath();
             for (let i = 0; i < normalizedY.length - 1; i++) {
-                ctx.moveTo(i, normalizedY[i] * canvasHeight);
-                ctx.lineTo(i + 1, normalizedY[i + 1] * canvasHeight);
-                ctx.stroke();
+                line(ctx,i * spacing, canvasHeight-(normalizedY[i] * canvasHeight),
+                    (i+1)*spacing, canvasHeight-(normalizedY[i + 1] * canvasHeight));
             }
+
+            line(ctx,30,0,30,canvasHeight-30);
+
 
             console.log("Done drawing");
             return canvas;
         }
     },
 };
+
+function line(context,x1,y1,x2,y2) {
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+}
 
 function normalize(array) {
     const min = Math.min(...array);
